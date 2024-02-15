e9595bd1fc01df4259fd80372072e58b4d39b468
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2024-02-15 15:14:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,29 @@
-import json
-import uuid
-from sentence_transformers import SentenceTransformer
-from pinecone import Pinecone
-
+import os
+from langchain.document_loaders import OnlinePDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_pinecone import Pinecone
 
 class InferlessPythonModel:
     def initialize(self):
-        self.pipe = SentenceTransformer("all-MiniLM-L6-v2")
-        pc = Pinecone(api_key="31b47ff0-5126-4f21-9d55-8ea2714e1a7d")
-        self.index = pc.Index("documents")
+        #define the index name of Pinecone, embedding model name and pinecone API KEY
+        index_name = "documents"
+        embed_model_id = "sentence-transformers/all-MiniLM-L6-v2"
+        os.environ["PINECONE_API_KEY"] = "31b47ff0-5126-4f21-9d55-8ea2714e1a7d"
+
+        #Initialize the embedding model, text_splitter & pinecone
+        embeddings=HuggingFaceEmbeddings(model_name=embed_model_id)
+        self.text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+        self.pinecone = Pinecone(index_name=index_name, embedding=embeddings)
 
     def infer(self, inputs):
-      data = inputs["data"]
-      request_type = inputs["type"]
+      pdf_link = inputs["pdf_url"]
+      loader = OnlinePDFLoader(pdf_link)
+      data = loader.load()
+      documents = self.text_splitter.split_documents(data)
+      response = self.pinecone.add_documents(documents)
+      
+      return {"result":response}
 
-      if request_type=="upload":
-        response = self.upload(data)
-      else:
-        query = inputs["data"][0]
-        response = self.search(query.decode())
-
-      return {"result":str(response)}
-
-    def upload(self,data):
-      upload_data = []
-      for sentence in data:
-          vectors = self.pipe.encode(sentence.decode())
-          vector_data = {
-                          "id": str(uuid.uuid4()),
-                          "values": vectors.tolist()
-                          #"metadata": {"document_category": data_row["metadata"]["category"]} #You can use store the metadata along with the vectors
-                          }
-          upload_data.append(vector_data)
-
-      response = self.index.upsert(
-                  vectors=upload_data,
-                  namespace= "ns1")
-      return response
-
-    def search(self,query):
-      vectors = self.pipe.encode(query)
-      response = self.index.query(
-                        namespace="ns1",         # Change the namespace according to your requirements
-                        vector=vectors.tolist(), # Vectors needs to convert into a list during the search
-                        top_k=2,                 # This will determine the number of documents that needs to be return
-                        include_values=True,
-                        include_metadata=True
-                    )
-      return response
-
-    def finalize(self, args):
-        self.pipe = None
-        self.index = None
+    def finalize(self):
+      pass
